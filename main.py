8e307e3555a41fb04215dfe662e4748ff5eb9866
FernandaLorca <fernanda.lorca_b@mail.udp.cl>
2022-03-26 19:42:30 -0300

    modificacion admin

--- main.py
+++ main.py
@@ -24,63 +24,42 @@ def index():
 
     return render_template('index.html', ans=ans)
 
-@app.route('/pagina')
-def pagina():
-    var = 'Esta es la pagina secundaria'
-    return render_template('pagina.html', var=var)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method=='GET':
         return render_template('admin_login.html')
     else:
+
         user = request.form.get("password")
 
         if user=='sii123':
+
+            conectar = connectDB()
+            cursor=conectar.cursor()
+            cursor.execute("""SELECT desde, hasta, factor, CR
+                            FROM IA_2022; """)
         
-            conectar = connectDB()
-            cursor=conectar.cursor()
-            cursor.execute(f"""SELECT c.rut, c.nombre, c.edad, count(p.rut_cliente)
-                            FROM cliente AS c, pedido AS p
-                            WHERE c.rut='{user}'
-                            AND c.rut=p.rut_cliente
-                            GROUP BY c.rut;""")
-
-            cliente=cursor.fetchall()
-
-        if cliente :
+            datos=cursor.fetchall()
 
             conectar = connectDB()
             cursor=conectar.cursor()
-            cursor.execute(f"""SELECT DISTINCT(t.nombre)
-                            FROM cliente AS c, trabajador as t, tra_cli as tc
-                            WHERE c.rut='{user}'
-                            AND c.rut=tc.rut_cliente 
-                            AND t.id_trabajador = tc.id_trabajador;""")
-            
-            trabajadores=cursor.fetchall()
+            cursor.execute("""SELECT mes_v, porcentaje
+                            FROM CM_2021
+                            WHERE mes_h='Diciembre'; """)
 
-            conectar = connectDB()
-            cursor=conectar.cursor()
-            cursor.execute(f"""SELECT SUM(f.total)
-                            FROM factura AS f, pedido as p, cliente AS c
-                            WHERE c.rut=p.rut_cliente 
-                            AND f.codigo_pedido = p.codigo_pedido
-                            AND c.rut='{user}';""")
-            
-            total=cursor.fetchall()
+            cm=cursor.fetchall()
 
             ctx = {
-            "cliente" : cliente,
-            "trabajadores" : trabajadores,
-            "total" : total
+            "datos" : datos,
+            "cm" : cm
             }
-            
-            return render_template('index_cliente.html', contexto=ctx)
+
+            return render_template('admin.html', contexto=ctx)
 
         else:
-            flash('Rut invalido', 'warning')
-            return redirect(url_for('cliente'))
+             return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
