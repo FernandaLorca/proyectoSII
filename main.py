cd5abb1be0cfdd264917ba7843241f231c4ffbff
FernandaLorca <fernanda.lorca_b@mail.udp.cl>
2022-03-31 14:44:23 -0300

    pa calcular añadida

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from termios import CR3
 from flask import Flask, render_template, request, redirect, url_for, flash
 import psycopg2
 import psycopg2.extras
 import os
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 def connectDB():
     connectionString='dbname=taxhelp user=fer password=ferfer host=localhost'
@@ -17,19 +16,30 @@ def connectDB():
 
 @app.route('/')
 def index():
+
     conectar = connectDB()
-    cursor = conectar.cursor()
-    cursor.execute("""SELECT factor FROM IA;""")
-    
+    cursor=conectar.cursor()
+    cursor.execute("""SELECT distinct anio FROM IA;""")
+        
+    anios=cursor.fetchall()
+
+    return render_template('index.html', anios=anios)
+
+@app.route('/calculo/<int:anio>')
+def calculo(anio):
+
+    conectar = connectDB()
+    cursor=conectar.cursor()
+    cursor.execute(f"""SELECT * FROM IA WHERE anio={anio};""")
+        
     ans=cursor.fetchall()
-
-    return render_template('index.html', ans=ans)
-
+   
+    return render_template('calculo.html', ans=ans)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method=='GET':
-        return render_template('adminLog.html')
+        return render_template('adminlogin.html')
     else:
 
         user = request.form.get("password")
@@ -39,14 +49,14 @@ def admin():
             conectar = connectDB()
             cursor=conectar.cursor()
             cursor.execute("""SELECT desde, hasta, factor, CR, anio
-                            FROM IA; """)
+                            FROM IA order by anio, factor; """)
         
             datos=cursor.fetchall()
 
             conectar = connectDB()
             cursor=conectar.cursor()
             cursor.execute("""SELECT mes, porcentaje, anio
-                            FROM CM; """)
+                            FROM CM order by anio; """)
 
             cm=cursor.fetchall()
 
@@ -60,108 +70,165 @@ def admin():
         else:
              return render_template('index.html')
 
-@app.route('/crearCM', methods=['GET', 'POST'])
-def crearCM():
+@app.route('/ingresarcm', methods=['GET', 'POST'])
+def ingresarcm():
+
     if request.method=='GET':
-        return render_template('añadirCM.html')
+        return render_template('ingresarcm.html')
+
     else:
 
-        enero = request.form.get("P.enero")
-        febrero = request.form.get("P.febrero")
-        marzo = request.form.get("P.marzo")
-        abril = request.form.get("P.abril")
-        mayo = request.form.get("P.mayo")
-        junio = request.form.get("P.junio")
-        julio = request.form.get("P.julio")
-        agosto = request.form.get("P.agosto")
-        septiembre = request.form.get("P.septiembre")
-        octubre = request.form.get("P.octubre")
-        noviembre = request.form.get("P.noviembre")
+        mes = request.form.get("mes")
+        porcentaje = request.form.get("porcentaje")
         anio = request.form.get("anio")
-        
+
+        sql = 'INSERT INTO CM (mes, porcentaje, anio) VALUES(%s,%s,%s)'
+
+        datos = (mes, porcentaje, anio)
+
         conectar = connectDB()
         cursor=conectar.cursor()
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {enero}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {febrero}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {marzo}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {abril}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {mayo}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {junio}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {julio}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {agosto}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {septiembre}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {octubre}, {anio}); """)
-        cursor.execute(f"""insert into CM (mes,  porcentaje, anio) values ('Enero', {noviembre}, {anio}); """)
 
+        print(sql)
+
+        try:
+            cursor.execute(sql,datos)
+            conectar.commit()
+            return render_template('ingresarcm.html')
+        except:
+            return render_template('ingresarcm.html')
     
-        return render_template('admin.html')
 
-@app.route('/crearIA', methods=['GET', 'POST'])
-def crearIA():
+@app.route('/ingresaria', methods=['GET', 'POST'])
+def ingresaria():
+
     if request.method=='GET':
-        return render_template('añadirIA.html')
+        return render_template('ingresaria.html')
+
     else:
-        #desde = []
-        #for i in range (8):
-        #    desde[i+1] = request.form.get("desde"+[i+1])
 
-        desde1 = request.form.get("desde1")
-        desde2 = request.form.get("desde2")
-        desde3 = request.form.get("desde3")
-        desde4 = request.form.get("desde4")
-        desde5 = request.form.get("desde5")
-        desde6 = request.form.get("desde6")
-        desde7 = request.form.get("desde7")
-        desde8 = request.form.get("desde8")
-
-        hasta1 = request.form.get("hasta1")
-        hasta2 = request.form.get("hasta2")
-        hasta3 = request.form.get("hasta3")
-        hasta4 = request.form.get("hasta4")
-        hasta5 = request.form.get("hasta5")
-        hasta6 = request.form.get("hasta6")
-        hasta7 = request.form.get("hasta7")
-        #hasta8 = request.form.get("hasta8")
-
-        factor1 = request.form.get("factor1")
-        factor2 = request.form.get("factor2")
-        factor3 = request.form.get("factor3")
-        factor4 = request.form.get("factor4")
-        factor5 = request.form.get("factor5")
-        factor6 = request.form.get("factor6")
-        factor7 = request.form.get("factor7")
-        factor8 = request.form.get("factor8")
-
-        cr1 = request.form.get("factor1")
-        cr2 = request.form.get("factor2")
-        cr3 = request.form.get("factor3")
-        cr4 = request.form.get("factor4")
-        cr5 = request.form.get("factor5")
-        cr6 = request.form.get("factor6")
-        cr7 = request.form.get("factor7")
-        cr8 = request.form.get("factor8")
-
+        desdeIA = request.form.get("desdeIA")
+        hastaIA = request.form.get("hastaIA")
+        factorIA = request.form.get("factorIA")
+        cr = request.form.get("CR")
         anio = request.form.get("anio")
-        
+
+        sql = 'INSERT INTO IA (desde, hasta, factor, CR, anio) VALUES(%s,%s,%s,%s,%s)'
+
+        datos = (desdeIA, hastaIA, factorIA, cr, anio)
+
         conectar = connectDB()
         cursor=conectar.cursor()
 
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde1}, {hasta1}, {factor1}, {cr1}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde2}, {hasta2}, {factor2}, {cr2}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde3}, {hasta3}, {factor3}, {cr3}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde4}, {hasta4}, {factor4}, {cr4}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde5}, {hasta5}, {factor5}, {cr5}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde6}, {hasta6}, {factor6}, {cr6}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde7}, {hasta7}, {factor7}, {cr7}, {anio});""")
-        cursor.execute(f"""insert into IA (desde, hasta, factor, CR, anio) values ({desde8}, null, {factor8}, {cr8}, {anio});""")
-
+        cursor.execute(sql, datos)
 
         conectar.commit()
-        print("Records inserted successfully")
-        conectar.close()
     
-        return render_template('admin.html')
+    return render_template('ingresaria.html')
 
+@app.route('/eliminaria', methods=['GET', 'POST'])
+def eliminaria():
+
+    conectar = connectDB()
+    cursor=conectar.cursor()
+    cursor.execute("""SELECT desde, hasta, factor, CR, anio
+                            FROM IA order by anio, factor; """)
+        
+    datos=cursor.fetchall()
+
+    conectar = connectDB()
+    cursor=conectar.cursor()
+    cursor.execute("""SELECT mes, porcentaje, anio
+                       FROM CM order by anio; """)
+
+    cm=cursor.fetchall()
+
+    ctx = {
+        "datos" : datos,
+        "cm" : cm
+            }
+
+    if request.method=='GET':
+        return render_template('eliminaria.html', contexto=ctx)
+
+    else:
+
+        desdeIA = request.form.get("desdeIA")
+        hastaIA = request.form.get("hastaIA")
+        factorIA = request.form.get("factorIA")
+        cr = request.form.get("CR")
+        anio = request.form.get("anio")
+
+        sql = 'DELETE FROM IA WHERE desde=%s AND hasta=%s AND factor=%s AND CR=%s AND anio=%s'
+
+        datos = (desdeIA, hastaIA, factorIA, cr, anio)
+
+        conectar = connectDB()
+        cursor=conectar.cursor()
+
+        print(sql)
+
+        cursor.execute(sql,datos)
+        conectar.commit()
+        return render_template('admin.html', contexto=ctx)
+        
+
+
+@app.route('/eliminarcm', methods=['GET', 'POST'])
+def eliminarcm():
+
+    conectar = connectDB()
+    cursor=conectar.cursor()
+    cursor.execute("""SELECT desde, hasta, factor, CR, anio
+                            FROM IA order by anio, factor; """)
+        
+    datos=cursor.fetchall()
+
+    conectar = connectDB()
+    cursor=conectar.cursor()
+    cursor.execute("""SELECT mes, porcentaje, anio
+                       FROM CM order by anio; """)
+
+    cm=cursor.fetchall()
+
+    ctx = {
+        "datos" : datos,
+        "cm" : cm
+            }
+
+    if request.method=='GET':
+        return render_template('eliminarcm.html', contexto=ctx)
+
+    else:
+
+        mes = request.form.get("mes")
+        porcentaje = request.form.get("porcentaje")
+        anio = request.form.get("anio")
+
+        sql = 'DELETE FROM CM WHERE mes=%s AND porcentaje=%s AND anio=%s'
+
+        datos = (mes, porcentaje, anio)
+
+        conectar = connectDB()
+        cursor=conectar.cursor()
+
+        print(sql)
+
+        try:
+            cursor.execute(sql,datos)
+            conectar.commit()
+            return render_template('admin.html', contexto=ctx)
+        except:
+            return render_template('admin.html', contexto=ctx)
+
+@app.route('/calcular',  methods=['POST'])
+def calcular():
+
+    
+   
+    return render_template('calcular.html')
+
+  
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
